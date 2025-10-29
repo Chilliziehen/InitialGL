c8869808f5a55ac9d0591e4ab4eade6233e2c677
Chilliziehen <303677483@qq.com>
2025-10-29 11:55:01 +0800

    Init
    初始仓库

---
+++ config/config.h
@@ -0,0 +1,8 @@
+//
+// Created by 30367 on 2025/10/28.
+//
+#pragma once
+
+#define RESOURCE_PATH "D:/Repositories/ComputerGraphics/2025_autumn_cg/src/cxx/Resources"
+
+#define GLSL_PATH "D:/Repositories/ComputerGraphics/2025_autumn_cg/src/glsl"
